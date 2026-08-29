d2aac919665d2fdf18d223e78ecc6f432eddc231
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 12:00:36 +0300

    sm6375-common: Convert remaining overlays to standalone RRO
    Change-Id: I70fc1fb9e8742b8e9b18bd2261673ca3ad8d6346

--- common.mk
+++ common.mk
@@ -22,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
@@ -33,8 +30,11 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     Launcher3DeviceOverlay \
     LineageApertureAppCommon \
+    LineageSdkOverlayCommon \
+    LineageSettingsProviderOverlayCommon \
     NcmTetheringOverlay \
     SettingsProviderResCommon \
+    SettingsResCommon \
     SystemUIResCommon \
     TelephonyResCommon \
     TelephonyResProduct \
